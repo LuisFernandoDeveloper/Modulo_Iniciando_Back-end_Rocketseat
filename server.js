e0e3a249f90f4d2a8492bf5a3307330aae85d5bd
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-05-13 00:58:52 -0300

    Criando rotas e instalando nodemon para rebutar o servidor

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get("/", function(req, res){
+    return res.send("Hello World 2")
+})
+
 server.listen(5000, function(){
     console.log("server is running")
 })
