82c56da1671f87210d8b8b84cfc32422ad055fc8
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-05-13 01:22:19 -0300

    Instalando e configurando Nunjucks, Trazendo arquivos do modulos frontend para o servidor

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express:server
+})
+
 server.get("/", function(req, res){
-    return res.send("Hello World 2")
+    return res.render("index")
+})
+
+server.get("/classes", function(req, res){
+    return res.render("classes.html")
 })
 
 server.listen(5000, function(){
