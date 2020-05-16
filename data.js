dafc75af3e5b2bca293f9a5cb9bb1bd647e906d6
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-05-16 20:53:04 -0300

    Passando dados do Back para o Front

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "EhnXaybirdA",
+        title: "Criando um projeto do zero para seu portfólio | Masterclass #14",
+        durarion: "45 min",
+        price: "Free"
+    },
+    {
+        id: "KISMYYXSIX8",
+        title: "Autenticação no React Native / ReactJS com Context API & Hooks | Masterclass #12",
+        duration: "71 min",
+        price: "Free"
+    },
+    {
+        id: "BvhYm0BOLvA",
+        title: "Dark Mode raiz com CSS e JavaScript | Code/Drops #24",
+        duration: "17 min",
+        price: "Free"
+    },
+    {
+        id: "mxIhSTP6ddE",
+        title: "Utilizando UX para projetar uma aplicação do zero | Masterclass #09",
+        duration: "79 min",
+        price: "Free"
+    },
+    {
+        id: "eSjFDWYkdxM",
+        title: "Emulando React Native no iOS/Android com Expo | Code/Drops #03",
+        duration: "16 min",
+        price: "Free"
+    }
+]
