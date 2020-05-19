7e8136e7caacd79180444e55dbacf1bf0730141a
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-05-19 02:47:40 -0300

    Iniciando desafio 3-2: Arquivos nunjucks e dados dinâmicos

---
+++ desafio3.2/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", function(req, res){
+    const data = {
+        logo: "img/logo.jpg",
+        company: "Rocketseat",
+        phrase: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description: "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+        courses = [
+            { name: "NodeJS" },
+            { name: "React Native" },
+            { name: "React JS" }
+        ],
+        links = [
+            { name: "Github", url: "/" },
+            { name: "Instagram", url: "/" },
+            { name: "Facebook", url: "/" }
+        ]
+    }
+    return res.render("index", { data })
+})
+
+server.get("/courses", function(req, res){
+    return res.render("courses")
+})
+
+server.use(function(req, res){
+    res.status(404).render("not-found")
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
