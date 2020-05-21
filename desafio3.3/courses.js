7fe4a5c7f211f062627fb029e3db7914e9644dee
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-05-20 22:47:09 -0300

    desafio 3-3 finalizado

---
+++ desafio3.3/courses.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id:"starter",
+        url:"img/logostarter.jpg",
+        name:"Starter",
+        module: "5 modulos",
+        price: "Free"
+    },
+    {
+        id:"launchbase",
+        url:"/img/logolaunchbase.png",
+        name:"Launchbase",
+        module: "10 modulos",
+        price: "Free"
+    },
+    {
+        id:"gostack",
+        url:"img/logogoStack.PNG",
+        name:"GoStack",
+        module: "13 modulos",
+        price: "Free"
+    },
+]
---
+++ desafio3.3/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const courses = require("./courses")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res){
+    const data = {
+        logo: "img/logo.jpg",
+        company: "Rocketseat",
+        phrase: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description: "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+        courses: [
+            { name: "NodeJS" },
+            { name: "React Native" },
+            { name: "React JS" }
+        ]
+    }
+    return res.render("index", { data })
+})
+
+server.get("/courses", function(req, res){
+    return res.render("courses", { courses })
+})
+
+server.get("/course", function(req, res){
+    const id = req.query.id
+
+    const course = courses.find(function(course){
+        if(course.id == id){
+            return true
+        }
+    })
+    if(!course){
+        return res.send("Course not found!")
+    }
+    return res.render("course", {course})
+})
+
+server.use(function(req, res){
+    res.status(404).render("not-found")
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
